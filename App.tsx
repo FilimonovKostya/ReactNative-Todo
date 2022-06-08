06cc3da14a54426c245f991953d2ddbdba536f04
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-06-08 11:58:17 +0400

    added libs for native base

--- App.tsx
+++ App.tsx
@@ -1,12 +1,50 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button, NativeBaseProvider } from 'native-base'
+
+type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
 
 export default function App() {
+  const [tasks, setTasks] = React.useState<TaskType[]>([
+    {
+      id: 1,
+      title: 'React',
+      isDone: true,
+    },
+    {
+      id: 2,
+      title: 'React Native',
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: 'ApolloClient',
+      isDone: false,
+    },
+    {
+      id: 4,
+      title: 'Redux',
+      isDone: true,
+    },
+  ])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {tasks.map((task, index) => (
+        <View key={index}>
+          <Text>{task.title}</Text>
+          <Text>{task.isDone}</Text>
+        </View>
+      ))}
+      <NativeBaseProvider>
+        <Button onPress={() => console.log('hello world')}>Click Me</Button>
+      </NativeBaseProvider>
       <StatusBar style='auto' />
-      <Text>daad</Text>
     </View>
   )
 }
