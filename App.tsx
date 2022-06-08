f461610f09f788190397ed2d7e41aac0d20d2ae6
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-06-08 13:46:14 +0400

    change styles of todo

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Checkbox from 'expo-checkbox'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import { Checkbox, NativeBaseProvider } from 'native-base'
+import { NativeBaseProvider } from 'native-base'
 
 type TaskType = {
   id: number
@@ -28,14 +29,7 @@ export default function App() {
     },
   ])
   const changeStatusTask = (id: number, isDone: boolean) => {
-    const newStatusTask = tasks.map((task) =>
-      task.id === id
-        ? {
-            ...task,
-            isDone,
-          }
-        : { ...task }
-    )
+    const newStatusTask = tasks.map((task) => (task.id === id ? { ...task, isDone } : task))
     setTasks(newStatusTask)
   }
 
@@ -45,18 +39,12 @@ export default function App() {
       <View style={styles.container}>
         <View>
           {tasks.map((task, index) => (
-            <View key={index}>
-              <Text>{task.isDone}</Text>
+            <View key={task.id} style={styles.row}>
               <Checkbox
-                value={task.title}
-                isChecked={task.isDone}
-                onChange={(isSelected) => {
-                  changeStatusTask(task.id, isSelected)
-                }}
-                accessibilityLabel='This is a dummy checkbox'
-              >
-                {task.title}
-              </Checkbox>
+                value={task.isDone}
+                onValueChange={(value) => changeStatusTask(task.id, value)}
+              />
+              <Text style={styles.text}>{task.title}</Text>
             </View>
           ))}
         </View>
@@ -69,7 +57,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#cde1ff',
+    paddingVertical: 8,
+    marginVertical: 10,
+    paddingHorizontal:10,
+  },
+  text: {
+    color: '#2229b2',
+    fontSize: 20,
+    marginLeft: 10,
+    fontWeight: 'bold',
   },
 })
